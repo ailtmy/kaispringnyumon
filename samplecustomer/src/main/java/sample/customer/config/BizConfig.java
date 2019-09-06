bc69966e2be6a25d11bffffd26f2504b5e4d798e
takase <takase@mitsuwa-off.jp>
2019-09-06 18:52:37 +0900

    chap5-2-2

---
+++ samplecustomer/src/main/java/sample/customer/config/BizConfig.java
@@ -0,0 +1,33 @@
+package sample.customer.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+@ComponentScan("sample.customer.biz.service")
+public class BizConfig {
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource
+			= new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:/META-INF/messages");
+
+		return messageSource;
+	}
+
+	@Bean
+	public Validator globalValidator(MessageSource messageSource) {
+		LocalValidatorFactoryBean validatorBean
+			= new LocalValidatorFactoryBean();
+		validatorBean.setValidationMessageSource(messageSource);
+
+		return validatorBean;
+	}
+
+}
